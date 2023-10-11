6b300b8a53aac4bb6abbb55c876dd10c35230043
Matthew McAuley <mattwilliam618@gmail.com>
2023-10-11 16:49:47 -0400

    Completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
 
+    for (let i = 1; i < arguments.length; i++) {
+        let index = arr.indexOf(arguments[i]);
+        if (index >= 0) {
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
