a8f62cc688a3db8c2887c36a16f1d0ecdce3e8c1
Matthew McAuley <mattwilliam618@gmail.com>
2023-10-26 18:30:03 -0400

    Finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+    let sum = 0;
 
+    if (n1 < 0 || n2 < 0 || typeof(n1) != "number" || typeof(n2) != "number") {
+        return "ERROR";
+    }
+
+    if (n1 > n2) {
+        larger = n1;
+        smaller = n2;
+    } else {
+        larger = n2;
+        smaller = n1;
+    }
+
+    for (let i = smaller; i <= larger ; i++) {
+        sum += i;
+    }
+
+    return sum
 };
 
 // Do not edit below this line
