a679f03b93e1ae883b4ad550ffb94ea7f3047314
Matthew McAuley <mattwilliam618@gmail.com>
2023-11-08 22:33:02 -0500

    completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
+    if (num <= 0) {
+        return "OOPS";
+    }
 
+    num1 = 1;
+    num2 = 1;
+    if (num == 1 || num == 2) {
+        return 1;
+    }
+
+    for (let i = 3; i < num; i++) {
+        tmp = num1;
+        num1 = num2;
+        num2 = tmp + num2;
+    }
+
+    return num1 + num2;
 };
 
 // Do not edit below this line
