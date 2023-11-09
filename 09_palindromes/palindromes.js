afca9c6cbd4a357d09397f2c79a72d1e4b310ef9
Matthew McAuley <mattwilliam618@gmail.com>
2023-11-08 22:22:37 -0500

    Finished exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    const arr = string.split('');
+    const newArr = arr.filter(letter => {
+        return ('abcdefghijklmnopqrstuvwxyz1234567890'.includes(letter));
+    })
+    let l = 0;
+    let r = newArr.length-1;
 
+    while (l < r) {
+        if (newArr[l] != newArr[r]) {
+            return false;
+        }
+        l += 1;
+        r -= 1;
+    }
+    
+    return true;
 };
 
 // Do not edit below this line
