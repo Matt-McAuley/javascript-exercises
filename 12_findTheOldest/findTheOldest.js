b4a539bb1cec61f3730c2de06833903b6bd093bb
Matthew McAuley <mattwilliam618@gmail.com>
2023-11-09 21:01:20 -0500

    Finished exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const sortedAges = arr.sort((person1, person2) => {
+        if (person1.yearOfDeath === undefined && person2.yearOfDeath === undefined) {
+            return ((2023 - person1.yearOfBirth) > (2023 - person2.yearOfBirth)) ? -1 : 1;
+        }
+        if (person1.yearOfDeath === undefined) {
+            return ((2023 - person1.yearOfBirth) > (person2.yearOfDeath - person2.yearOfBirth)) ? -1 : 1;
+        }
+        if (person2.yearOfDeath === undefined) {
+            return ((person1.yearOfDeath - person1.yearOfBirth) > (2023 - person2.yearOfBirth)) ? -1 : 1;
+        }
+        return ((person1.yearOfDeath - person1.yearOfBirth) > (person2.yearOfDeath - person2.yearOfBirth)) ? -1 : 1;
+    });
+    return sortedAges[0];
 };
 
 // Do not edit below this line
